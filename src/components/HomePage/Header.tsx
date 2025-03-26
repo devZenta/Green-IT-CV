5d6c44de6c55ef1505604f204e4c2b2d711d8c5f
Hugo Ghesquier <hugo.ghesquier59@gmail.com>
2025-03-26 16:59:50 +0100

    Add Quote and ScrambleText components; update Header to include Quote

--- src/components/HomePage/Header.tsx
+++ src/components/HomePage/Header.tsx
@@ -1,9 +1,11 @@
 import { Navbar } from "../Utils/Navbar";
+import { Quote } from "../Utils/Quote";
 
 export function Header() {
   return (
     <div>
-      <Navbar />
+        <Navbar />
+        <Quote />
     </div>
   )
 }
