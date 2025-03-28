9520c4cbcf66eaea16b06fc8b026ef0f1a128962
Hugo Ghesquier <hugo.ghesquier59@gmail.com>
2025-03-28 14:28:38 +0100

    Update QuoteContent component styling and text; adjust layout and margins

--- src/components/Utils/Quote.tsx
+++ src/components/Utils/Quote.tsx
@@ -1,8 +1,8 @@
 export function QuoteContent() {
   return (
-    <div className="relative z-20 w-full px-4 max-w-5xl mx-auto my-10 md:my-16 rounded-[40px] py-6 md:p-10 xl:py-20 flex flex-col items-center">
-      <div className="w-full flex justify-start pl-16 md:pl-28 lg:pl-36">
-        <p className="md:text-md text-xs text-red-500 lg:text-lg xl:text-xl">
+    <div className="relative z-20 w-full px-4 max-w-7xl mx-auto mt-20 md:mt-32 lg:mt-40 mb-10 md:mb-16 rounded-[40px] py-12 md:py-16 xl:py-28 flex flex-col items-center">
+      <div className="w-full flex justify-start pl-10 md:pl-16 lg:pl-24">
+        <p className="md:text-md text-xs text-red-500 lg:text-lg xl:text-xl ml-24 md:ml-32 lg:ml-44 xl:ml-52 self-start">
           Hello there
         </p>
       </div>
@@ -16,7 +16,7 @@ export function QuoteContent() {
           <p className="font-thin">skills and</p>
         </div>
         <div className="flex gap-1 md:gap-2 lg:gap-3 xl:gap-4 justify-center">
-          <p className="font-thin">expertise are</p>
+          <p className="font-thin">projects are</p>
           <h1 className="font-semibold">showcased in</h1>
         </div>
         <h1 className="font-semibold">detail...&quot;</h1>
